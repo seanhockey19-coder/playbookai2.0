c2c9d885053e5475d652b786527e1ba2af9d8a4d
seanhockey19-coder <seanhockey19@gmail.com>
2025-11-18 19:26:09 -0500

    Update tailwind.config.js for custom spacing
    Added custom spacing value to the Tailwind theme.

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,11 +2,15 @@
 module.exports = {
   content: [
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      spacing: {
+        "98": "26rem", // force rebuild
+      },
+    },
   },
   plugins: [],
 };
